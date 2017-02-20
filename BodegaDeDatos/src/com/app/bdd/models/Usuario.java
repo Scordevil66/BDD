ea46c1aaab857c315dd45d7251095d62824b4cac
ce <ce@ce-PC>
2017-02-20 14:35:05 -0500

    getters and seter, and some construsters

--- BodegaDeDatos/src/com/app/bdd/models/Usuario.java
+++ BodegaDeDatos/src/com/app/bdd/models/Usuario.java
@@ -10,7 +10,88 @@ package com.app.bdd.models;
  * @author user
  */
 public class Usuario {
-    
-    private int idUsuario;
-    
+
+    private int intIdUsuario;
+
+    private String varNombreUser;
+
+    private String varApellidosUser;
+
+    private String varCargoUser;
+
+    private String varUsuario;
+
+    private String varPassword;
+
+//Constructers
+    public Usuario() {
+    }
+
+    public Usuario(int intIdUsuario) {
+        this.intIdUsuario = intIdUsuario;
+    }
+
+    public Usuario(int intIdUsuario, String varNombreUser, String varApellidosUser, String varCargoUser, String varUsuario, String varPassword) {
+        this.intIdUsuario = intIdUsuario;
+        this.varNombreUser = varNombreUser;
+        this.varApellidosUser = varApellidosUser;
+        this.varCargoUser = varCargoUser;
+        this.varUsuario = varUsuario;
+        this.varPassword = varPassword;
+    }
+
+    //getters and Setters
+    public int getIntIdUsuario() {
+        return intIdUsuario;
+    }
+
+    public void setIntIdUsuario(int intIdUsuario) {
+        this.intIdUsuario = intIdUsuario;
+    }
+
+    public String getVarNombreUser() {
+        return varNombreUser;
+    }
+
+    public void setVarNombreUser(String varNombreUser) {
+        this.varNombreUser = varNombreUser;
+    }
+
+    public String getVarApellidosUser() {
+        return varApellidosUser;
+    }
+
+    public void setVarApellidosUser(String varApellidosUser) {
+        this.varApellidosUser = varApellidosUser;
+    }
+
+    public String getVarCargoUser() {
+        return varCargoUser;
+    }
+
+    public void setVarCargoUser(String varCargoUser) {
+        this.varCargoUser = varCargoUser;
+    }
+
+    public String getVarUsuario() {
+        return varUsuario;
+    }
+
+    public void setVarUsuario(String varUsuario) {
+        this.varUsuario = varUsuario;
+    }
+
+    public String getVarPassword() {
+        return varPassword;
+    }
+
+    public void setVarPassword(String varPassword) {
+        this.varPassword = varPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "intIdUsuario=" + intIdUsuario + ", varNombreUser=" + varNombreUser + ", varApellidosUser=" + varApellidosUser + ", varCargoUser=" + varCargoUser + ", varUsuario=" + varUsuario + ", varPassword=" + varPassword + '}';
+    }
+
 }

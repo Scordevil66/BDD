4d108b6f819a117a7387ee31dbf9c2d46554aa99
user <user@user-PC>
2017-02-20 16:04:37 -0500

    actualizacion de sistema de hospitales

--- BodegaDeDatos/src/bodegadedatos/BodegaDeDatos.java
+++ BodegaDeDatos/src/bodegadedatos/BodegaDeDatos.java
@@ -5,6 +5,8 @@
  */
 package bodegadedatos;
 
+import com.app.bdd.form.frmLogin;
+
 /**
  *
  * @author user
@@ -15,7 +17,9 @@ public class BodegaDeDatos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       frmLogin miLogin = new frmLogin();
+        miLogin.setLocationRelativeTo(null);
+        miLogin.setVisible(true);
     }
     
 }

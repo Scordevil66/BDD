68f210615d207c877cc9d6aafefbbdab8b527d6d
user <user@user-PC>
2017-02-22 09:45:47 -0500

    actualizacion de sistema de hospitales

--- BodegaDeDatos/src/com/app/bdd/controller/MovimientoController.java
+++ BodegaDeDatos/src/com/app/bdd/controller/MovimientoController.java
@@ -8,6 +8,10 @@ package com.app.bdd.controller;
 
 import com.app.bdd.conexion.ConexionSQL;
 import com.app.bdd.models.Movimientos;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -114,5 +118,26 @@ public class MovimientoController {
         return mo;
 
     }
+    
+      public static void LeerArchivoMovimientoTxt() {
+        //Creamos un String que va a contener todo el texto del archivo
+        String texto = "";
 
+        try {
+//Creamos un archivo FileReader que obtiene lo que tenga el archivo
+            FileReader lector = new FileReader("C:\\RRHH\\Conexion.txt");
+
+//El contenido de lector se guarda en un BufferedReader
+            BufferedReader contenido = new BufferedReader(lector);
+
+//Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
+            while ((texto = contenido.readLine()) != null) {
+            
+                System.out.println(texto);
+            }
+        } //Si se causa un error al leer cae aqui
+        catch (Exception e) {
+            System.out.println("Error al leer");
+        }
+    }
 }
